dd299d812b2658b33a0b66aa6a4efa6a6fa5e1e3
ChuchoMontesinos <65583318+ChuchoMontesinos@users.noreply.github.com>
2020-06-03 18:44:22 -0500

    Update test.js

--- src/_test_/test.js
+++ src/_test_/test.js
@@ -7,7 +7,7 @@ describe('App', function () {
         expect(testFunction(2)).toBe(2);
         expect(testFunction(3)).toBe(6);
         expect(testFunction(5)).toBe(120);
-        expect(testFunction(8)).toBe(4320);
+        expect(testFunction(8)).toBe(40320);
         expect(testFunction(-5)).toBe(0);
     })
 })
