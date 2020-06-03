193b77011e958bffb6d3498764cfcad0441efa73
Oscar Martinez <oscarmartinez1998@hotmail.es>
2020-06-03 17:51:49 -0500

    Solucion, nota: al test de 8! le faltaba un cero

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const factorial = function (number) {
-    //aqui escribiras tu codigo
-}; 
+    if(number < 0) return 0;
+    if(number == 0 || number == 1) return 1;
+    
+    let oddNumber, aux;
+
+    if(number % 2 != 0) {
+        aux = number;
+        oddNumber = number - 1;
+    } else {
+        aux = 1;
+        oddNumber = number;
+    }
+
+    let res = 1;
+
+    for(let i = 1; i <= oddNumber / 2; i++){
+        res *= i * (oddNumber + 1 - i);
+    }
+
+    return res * aux;
+
+};
 
 module.exports = factorial;
