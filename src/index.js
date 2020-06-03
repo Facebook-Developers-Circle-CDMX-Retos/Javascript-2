29e42ff7026c4ebad10ecc59e34d3d2d724fe893
enrique26 <enriqueibilis@gmail.com>
2020-06-03 14:56:13 -0500

    prueba factorial de un numero
    prueba factorial de un numero

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const factorial = function (number) {
     //aqui escribiras tu codigo
+    if(!isNaN(number) || number<0){
+        return 0;
+    }
+    let factorrial = 0
+    ///factorrial de 0 es 1
+    if(number === 0){
+        return 1;
+    }
+    ///multiplicar los valores anteriores del numero dado
+    for(var x = 1;x<=number;x++){
+        if(factorrial === 0){
+            factorrial = x
+        }else{
+            factorrial *= x
+        }
+    }
+    return factorrial
 }; 
 
 module.exports = factorial;
