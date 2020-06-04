ae13931f4978e58a15d01719361aaea25c7ebb74
Roberto M <lmaciel1303@gmail.com>
2020-06-03 21:24:19 -0500

    Respusta del reto, en el test había un error, ya que el factorial de 8 no es 4320 sino 40320

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const factorial = function (number) {
-    //aqui escribiras tu codigo
+    if (number < 0)
+        return 0
+    else if (number <= 1)
+        return 1
+
+    return number * factorial(number - 1)
 }; 
 
 module.exports = factorial;
